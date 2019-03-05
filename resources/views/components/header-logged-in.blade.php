eec347348d185aaa933e8ee0fce34df60e4af01f
Adhitama Fikri <adhitama.fikri@ralali.com>
2019-03-05 16:55:46 +0700

    slight update

--- resources/views/components/header-logged-in.blade.php
+++ resources/views/components/header-logged-in.blade.php
@@ -25,7 +25,9 @@
                             <img alt="Image placeholder" src="{{ asset('/img/theme/team-1-800x800.jpg') }}">
                         </span>
                         <div class="media-body ml-2 d-none d-lg-block">
-                            <span class="mb-0 text-sm  font-weight-bold">Jessica Jones</span>
+                            <span class="mb-0 text-sm  font-weight-bold">
+                                {{ Auth::user()->name }}
+                            </span>
                         </div>
                     </div>
                 </a>
